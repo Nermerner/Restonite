161c9df450d1a51538c518ab2aa596797477941e
Uruloke <38957400+Uruloke@users.noreply.github.com>
2024-03-09 14:16:24 +0100

    Update variable names for radial slicer

--- Restonite/MaterialHelpers.cs
+++ Restonite/MaterialHelpers.cs
@@ -312,36 +312,36 @@ namespace Restonite
             var sphere = newMaterial.Points.Add();
 
             var posDriver = destination.AttachComponent<DynamicValueVariableDriver<float3>>();
-            posDriver.VariableName.Value = "Avatar/Statue.RadialStatuefy.GlobalPosition";
+            posDriver.VariableName.Value = "Avatar/Statue.RadialStatuefy.GlobalOriginPoint";
             posDriver.Target.ForceLink(sphere.Position);
 
             var edgeColorDriver = destination.AttachComponent<DynamicValueVariableDriver<colorX>>();
-            edgeColorDriver.VariableName.Value = "Avatar/Statue.RadialStatuefy.Config.BorderColor";
+            edgeColorDriver.VariableName.Value = "Avatar/Statue.Slicer.EdgeEmissiveColor";
             edgeColorDriver.Target.ForceLink(newMaterial.EmissionColorFrom);
             newMaterial.EmissionColorTo.Value = new colorX(0.0f);
 
             // This needs to be the case because of behavior if both are 0; if 0, defaults to To
             var displaceMagnitudeFromDriver = destination.AttachComponent<DynamicValueVariableDriver<float>>();
-            displaceMagnitudeFromDriver.VariableName.Value = "Avatar/Statue.RadialStatuefy.Config.MagnitudeFrom";
+            displaceMagnitudeFromDriver.VariableName.Value = "Avatar/Statue.RadialStatuefy.DisplaceMagnitudeFrom";
             displaceMagnitudeFromDriver.DefaultValue.Value = -0.001f;
             displaceMagnitudeFromDriver.Target.ForceLink(newMaterial.DisplaceMagnitudeFrom);
 
             var displaceMagnitudeToDriver = destination.AttachComponent<DynamicValueVariableDriver<float>>();
-            displaceMagnitudeToDriver.VariableName.Value = "Avatar/Statue.RadialStatuefy.Config.MagnitudeTo";
+            displaceMagnitudeToDriver.VariableName.Value = "Avatar/Statue.RadialStatuefy.DisplaceMagnitudeTo";
             displaceMagnitudeToDriver.DefaultValue.Value = 0.001f;
             displaceMagnitudeToDriver.Target.ForceLink(newMaterial.DisplaceMagnitudeTo);
 
             var displaceFromDriver = destination.AttachComponent<DynamicValueVariableDriver<float>>();
-            displaceFromDriver.VariableName.Value = "Avatar/Statue.RadialStatuefy.GlobalRadius2";
+            displaceFromDriver.VariableName.Value = "Avatar/Statue.RadialStatuefy.DisplaceFrom";
             displaceFromDriver.Target.ForceLink(newMaterial.DisplaceFrom);
             var displaceToDriver = destination.AttachComponent<DynamicValueVariableDriver<float>>();
-            displaceToDriver.VariableName.Value = "Avatar/Statue.RadialStatuefy.GlobalRadius1";
+            displaceToDriver.VariableName.Value = "Avatar/Statue.RadialStatuefy.DisplaceTo";
             displaceToDriver.Target.ForceLink(newMaterial.DisplaceTo);
             var emissionFromDriver = destination.AttachComponent<DynamicValueVariableDriver<float>>();
-            emissionFromDriver.VariableName.Value = "Avatar/Statue.RadialStatuefy.GlobalRadius2";
+            emissionFromDriver.VariableName.Value = "Avatar/Statue.RadialStatuefy.EmissionFrom";
             emissionFromDriver.Target.ForceLink(newMaterial.EmissionFrom);
             var emissionToDriver = destination.AttachComponent<DynamicValueVariableDriver<float>>();
-            emissionToDriver.VariableName.Value = "Avatar/Statue.RadialStatuefy.GlobalRadius";
+            emissionToDriver.VariableName.Value = "Avatar/Statue.RadialStatuefy.EmissionTo";
             emissionToDriver.Target.ForceLink(newMaterial.EmissionTo);
 
             newMaterial.OffsetFactor.Value = -0.1f;
