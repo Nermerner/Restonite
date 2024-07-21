9b9e75c47139c33383c64540d4719fcfb58b2ec4
Uruloke <38957400+Uruloke@users.noreply.github.com>
2024-07-21 17:55:48 +0200

    Fix file hash generation

--- Restonite/File Hashes.ps1
+++ Restonite/File Hashes.ps1
@@ -1,5 +1,2 @@
-"MD5:"
-$(CertUtil -hashfile bin\Release\Restonite.dll MD5)[1] -replace " ",""
-""
-"SHA256:"
-$(CertUtil -hashfile bin\Release\Restonite.dll SHA256)[1] -replace " ",""
+"**MD5:** {0}" -f $($(CertUtil -hashfile bin\Release\Restonite.nupkg MD5)[1] -replace " ","")
+"**SHA256:** {0}" -f $($(CertUtil -hashfile bin\Release\Restonite.nupkg SHA256)[1] -replace " ","")
